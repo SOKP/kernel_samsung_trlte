49c3cf553bdb1378fee9acc4106c763cae34048c
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-06-06 22:19:57 -0500

    cpufreq: interactive: fix compiling warnings
    The gcc warns like:
    
      cpufreq_interactive.c:745:6: warning: operation on 'ret' may be
    undefined [-Wsequence-point]
    
    Since sprintf(...) just return 1 (one character) in this case, ret
    should not changed.
    Just discarding the result of sprintf(...) .
    
    So this fix the build in Linaro GCC 4.9 or any other on GCC 4.9
    
    Change-Id: I186cdccd75934f382564b18e09e7cf8d01afcd44
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1025,7 +1025,7 @@ static ssize_t show_target_loads(
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
 #endif
-	ret += sprintf(buf + --ret, "\n");
+	sprintf(buf + ret - 1, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -1091,8 +1091,13 @@ static ssize_t show_above_hispeed_delay(
 	for (i = 0; i < nabove_hispeed_delay; i++)
 		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
 			       i & 0x1 ? ":" : " ");
+<<<<<<< HEAD
 #endif
 	ret += sprintf(buf + --ret, "\n");
+=======
+
+	sprintf(buf + ret - 1, "\n");
+>>>>>>> fc32b85... cpufreq: interactive: fix compiling warnings
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
